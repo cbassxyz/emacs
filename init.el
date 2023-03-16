cd0f7b993701c33ebbde5f1b2d83eec58f6a370a
cbassxyz <seabass@pop-os.localdomain>
2023-03-16 13:39:14 -0400

    use-package cleanup

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-7;;; init.el --- Initialization file for Emacs
+;;; init.el --- Initialization file for Emacs
 ;;; Commentary: Emacs Startup File --- initialization for Emacs
 ;;; Sebastian's Custom Emacs (so far)
 
@@ -10,86 +10,42 @@
 
 ;; Package Configurations
 
+
+;; lsp-mode
+
 (use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  :init  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (rustic-mode . lsp)
-         ;; if you want which-key integration
+	 ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
-
 (use-package lsp-ui :commands lsp-ui-mode)
 
-;; if you are helm user
-;; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; optionally if you want to use debugger
-;; (use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
-;; optional if you want which-key integration
-;; (use-package which-key
-;;    :config
-;;    (which-key-mode))
-
-(use-package lsp-ui)
+;; rustic
 
 (require 'rustic)
   (setq rustic-lsp-server 'rust-analyzer)
-
 (use-package rustic
   :custom
   (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer"))
   :init
   (rustic-mode))
 
+;; corfu
+
 (use-package corfu
-  ;; Optional customizations
-  ;; :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-  ;; Enable Corfu only for certain modes.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  ;; Recommended: Enable Corfu globally.
-  ;; This is recommended since Dabbrev can be used globally (M-/).
-  ;; See also `corfu-excluded-modes'.
   :init
   (global-corfu-mode))
-
-;; Enable auto completion and configure quitting
 (setq corfu-auto t
       corfu-quit-no-match t)
-
-;; A few more useful configurations...
 (use-package emacs
   :init
-  ;; TAB cycle if there are only few candidates
   (setq completion-cycle-threshold 3)
-
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
   (setq tab-always-indent 'complete))
+
+;; flycheck
 
 (use-package flycheck
   :ensure t
@@ -100,7 +56,8 @@
   :init
   (add-hook 'flycheck-mode-hook #'turn-on-flycheck-inline))
 
-;; Load the least ugly emacs theme
+;; theme
+
 (setq custom-safe-themes t)
 (load-theme 'wombat t)
 
